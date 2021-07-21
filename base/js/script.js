21f06267f25381ea08afe7eada6af74237199f2c
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-21 17:45:43 -0600

    8. Fading In Elements (and Fading Them Out Again)

--- base/js/script.js
+++ base/js/script.js
@@ -2,5 +2,14 @@ $(function () {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $(".red-box").fadeOut(2000);
+  // $(".red-box").fadeOut(2000);
+  // $(".green-box").fadeOut(2000);
+  // $(".red-box").fadeIn(1000);
+  // $(".red-box").fadeTo(1000, 0.5);
+  $(".red-box").fadeTo(1000, 0.2);
+  $(".green-box").fadeTo(3000, 0.5);
+  $(".blue-box").fadeTo(4000, 0.8);
+
+  $(".blue-box").fadeToggle();
+  $(".blue-box").fadeToggle();
 });
