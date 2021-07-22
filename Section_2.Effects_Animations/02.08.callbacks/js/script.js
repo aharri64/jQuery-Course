f27a6bf4d0a420d329aa63cc2c9da0eee091e405
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-21 19:48:47 -0600

    Timing Animations Using Callback Functions

--- Section_2.Effects_Animations/02.08.callbacks/js/script.js
+++ Section_2.Effects_Animations/02.08.callbacks/js/script.js
@@ -1,16 +1,13 @@
+// $(function () {
+//   $(".red-box").fadeTo(1000, 0, function () {
+//     alert("The callback is finished")
+//   })
 $(function () {
-
-  // Fade out red box over 100 second
-  $(".red-box").fadeTo(1000, 0, function() {
-
-    // This callback function is executed once outer animation finishes.
-    // So the green box starts fading out once the red box finished fading out.
-    $(".green-box").fadeTo(1000, 0, function() {
-
-      // Similarly, any code inside this function will start executing only once
-      // the green box finishes fading out (after 2 seconds)
+  $(".red-box").fadeTo(1000, 0, function () {
+    $(".green-box").fadeTo(1000, 0, function () {
       $(".blue-box").fadeTo(1000, 0);
     });
   });
-
+  $("p").fadeOut(1000);
+  $("p").delay(500).fadeIn(1000)
 });
