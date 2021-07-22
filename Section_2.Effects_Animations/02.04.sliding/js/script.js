08e52c9ce2c4825fae2762e411130e9681e69a62
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-21 19:17:24 -0600

    Sliding Elements Up And Down

--- Section_2.Effects_Animations/02.04.sliding/js/script.js
+++ Section_2.Effects_Animations/02.04.sliding/js/script.js
@@ -1,11 +1,10 @@
 $(function () {
-  // Slide element up to hide (over 2s)
-  $(".blue-box").slideUp(2000);
+  // $(".blue-box").slideUp(2000);
+  // $(".blue-box").slideDown(2000);
+  $(".blue-box").slideToggle(2000);
 
-  // Slide back down to show element
-  $(".blue-box").slideDown(2000);
 
-  // Toggle sliding up/down depending on current state
-  $(".blue-box").slideToggle(1000);
-  $(".blue-box").slideToggle(1000);
+  // making paragraphs disappear when
+  // $("p").hide();
+  // $("p").slideDown(2000);
 });
--- Section_2.Effects_Animations/02.09.signup-lightbox/js/script.js
+++ Section_2.Effects_Animations/02.09.signup-lightbox/js/script.js
@@ -1,6 +1,3 @@
 $(function () {
 
-  // Fade in lightbox after half a second
-  $(".lightbox").delay(500).fadeIn(1000);
-
 });
