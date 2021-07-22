21956358911a8e807c18148dcb9ae4b7f5d2a477
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-21 19:34:48 -0600

    Get Creative: Custom Animations!

--- Section_2.Effects_Animations/02.06.custom-animations/js/script.js
+++ Section_2.Effects_Animations/02.06.custom-animations/js/script.js
@@ -1,17 +1,12 @@
 $(function () {
-
-  // Animate various CSS properties of the blue box
   $(".blue-box").animate({
-    marginLeft: "200px",
-    opacity: "0",
-    height: "50px",
+    "margin-left": "+=300px",
+    "opacity": "0",
+    "height": "50px",
     width: "50px",
     marginTop: "25px"
   }, 1000);
-
-  // Animate font size of all paragraphs
   $("p").animate({
-    "font-size": "20px",
+    fontSize: "20px"
   }, 1000);
-
 });
