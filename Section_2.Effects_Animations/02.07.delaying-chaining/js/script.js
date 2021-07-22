160a9b81eab3c21370d12a65fe650eaa25e0498b
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-21 19:41:07 -0600

    Delaying and Chaining Animations

--- Section_2.Effects_Animations/02.07.delaying-chaining/js/script.js
+++ Section_2.Effects_Animations/02.07.delaying-chaining/js/script.js
@@ -1,13 +1,5 @@
 $(function () {
-
-  // Fade out red box immediately to 50% opacity (over 1 second)
-  $(".red-box").fadeTo(1000, 0.5);
-
-  // Wait 1 second, then fade out green box to 50% opacity (over 1 second)
-  // This already using function chaining to call .fadeTo() directly on the
-  // return value of .delay()
+  $(".red-box").fadeTo(1000, 0.2);
   $(".green-box").delay(1000).fadeTo(1000, 0.5);
-
-  // Chaining even more function calls to create animation
-  $(".blue-box").delay(2000).fadeTo(1000, 0.5).fadeTo(1000, 1.0).delay(1000).fadeOut();
+  $(".blue-box").delay(2000).fadeTo(1000, 0.7);
 });
