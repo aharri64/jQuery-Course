c524452608d6a9ac930c72351d593e693dd2c1cb
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 10:05:47 -0600

    Replacing Elements and Content

--- Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.02.replacing-elements/js/script.js
+++ Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.02.replacing-elements/js/script.js
@@ -1,3 +1,13 @@
 $(function () {
-  
+//   $("li").replaceWith("<li>replaced</li>")
+//   $("li").replaceWith(function() {
+//     return "<li>Replaced with function</li>";
+//   });
+    var firstListItem = $("li:first");
+    $("p").replaceWith(firstListItem);
+
+    var greenBox = "<div class='green-box'>Green Box Replacement</div>"
+    // $(".red-box, .blue-box").replaceWith(greenBox);
+
+    $(greenBox).replaceAll(".red-box, .blue-box")
 });
