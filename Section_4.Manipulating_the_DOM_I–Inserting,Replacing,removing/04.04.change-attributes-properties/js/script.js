c88df7fa24670f1b887c82dc6ef92551fb7c1a43
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 10:52:54 -0600

    Manipulating Attributes and Properties

--- Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.04.change-attributes-properties/js/script.js
+++ Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.04.change-attributes-properties/js/script.js
@@ -1,3 +1,18 @@
 $(function () {
+    // attr(), prop(), val()
+    // var specialLink = $('#special-link');
+    // console.log(specialLink.attr("href")); 
+    // console.log(specialLink.attr("title")); 
 
+    // specialLink.attr("href", "http://amirharrison.com")
+
+    // var checkbox = $("input:checkbox");
+    // console.log(checkbox.prop("checked"));
+    // console.log(checkbox.attr("checked"));
+
+    var textInput = $("input:text");
+    console.log(textInput.val());
+
+    var rangeInput = $("input[type='range']");
+    console.log(rangeInput.val());
 });
