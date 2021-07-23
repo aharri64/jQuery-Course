43a9962f64998a8f0771124b07bb602ab3b73c4c
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 09:43:11 -0600

    Adding New Elements to the DOM

--- Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.01.adding-elements/js/script.js
+++ Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.01.adding-elements/js/script.js
@@ -1,4 +1,20 @@
 $(function () {
+    // $("ul ul:first").append("<li>I'm gonna be the last sub item"</li>); 
+    // $("<li>I'm gonna be the last item</li>").appendTo($("ul ul:first"))
+    // $("ul ul").prepend("<li>I'm gonna be the first sub item</li>"); 
+    // $("<li>I'm gonna be the first item</li>").prependTo("ul ul:first")
+    // $("<h3>Amir Harrison</h3>").prependTo("#content");
+    
+    $(".blue-box").before("<div class='blue-box'>Blue Friend</div>");
+    $(".red-box").after("<div class='red-box'>Another Red</div>");
+    $(".green-box").after("<div class='green-box'>Another Green</div>");
 
+    // $(".blue-box").before(function() {
+        // return "<div class='blue-box'>Blue Box 2</div>";
+    // });
+
+    // $(".blue-box").before($(".red-box"));
+
+    $("p").after($("#list"));
 });
 
