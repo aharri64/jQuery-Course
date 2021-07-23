824ed80dc47d36a97803357fd7b8a273aaa0da53
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 10:24:51 -0600

    Removing Elements and Content

--- Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.03.removing-elements/js/script.js
+++ Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.03.removing-elements/js/script.js
@@ -1,3 +1,12 @@
 $(function () {
+    // $("li").remove();
+    // $("form").children().not("input:text, textarea, br").remove();
 
+    // $("li").detach();
+    // or
+    // var detachListItem = $("li").detach();
+    // $("#content").append(detachListItem);
+
+    // $("p:first").empty();
+    $(".red-box, .blue-box, .green-box").empty();
 });
