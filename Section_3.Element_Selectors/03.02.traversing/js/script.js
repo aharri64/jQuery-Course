5ceb96ebeefd02554e36f0a302faeae3691ffbfa
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-22 19:19:40 -0600

    jQuery Methods for Traversal

--- Section_3.Element_Selectors/03.02.traversing/js/script.js
+++ Section_3.Element_Selectors/03.02.traversing/js/script.js
@@ -1,4 +1,11 @@
 $(function () {
-
+    // $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").parents("body").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").parent("div").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").prev().css("background-color", "rgba(180, 180, 30, 0.8)");
+    $(":header").next().css("background-color", "rgba(180, 180, 30, 0.8)");
+    $("form").find(":password").css("background-color", "rgba(180, 180, 30, 0.8)");
 });
 
