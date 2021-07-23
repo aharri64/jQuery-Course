c7e38bb40a66dc9f6328ab589a5bae85dfd7d2dc
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-22 18:57:53 -0600

    Using CSS Selectors in jQuery

--- Section_3.Element_Selectors/03.01.selectors/js/script.js
+++ Section_3.Element_Selectors/03.01.selectors/js/script.js
@@ -1,5 +1,12 @@
 $(function () {
-
+    // $("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:first").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:even").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:odd").css("background-color", "rgba(180, 180, 30, 0.8)");
+    $("input:text").css("background-color", "rgba(180, 180, 30, 0.8)");
 });
-
 
