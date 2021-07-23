5191a9ddca28eb61741c3f9f87769d646ef945fa
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 11:21:17 -0600

    Coding Activity: Creating an Image Slideshow!

--- Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.05.image-slideshow/js/script.js
+++ Section_4.Manipulating_the_DOM_I–Inserting,Replacing,removing/04.05.image-slideshow/js/script.js
@@ -1,3 +1,18 @@
 $(function () {
+    var galleryImage = $(".gallery").find("img").first();
+    var images = [
+        "images/laptop-mobile_small.jpg",
+        "images/laptop-on-table_small.jpg",
+        "images/people-office-group-team_small.jpg",
+    ];
 
+    var i = 0;
+    setInterval(function () {
+        i = (i + 1) % images.length; // 0, 1, 2, 0, 1, 2, ... 
+        galleryImage.fadeOut(function () {
+            $(this).attr("src", images[i]);
+            $(this).fadeIn();
+        });
+        console.log(galleryImage.attr("src"));
+    }, 2000);
 });
