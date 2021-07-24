0d8eb3c39757bfe97cbbe897f8831f1fba31de74
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-24 16:03:34 -0600

    Retrieving and Changing the Content of an Element

--- Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.04.changing-element-content/js/script.js
+++ Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.04.changing-element-content/js/script.js
@@ -1,3 +1,12 @@
 $(function () {
+    // text(), html()
+    var firstParagraph = $("p:first");
+    console.log(firstParagraph.text());
+    console.log(firstParagraph.html());
+
+    // firstParagraph.text("<string>Hello</strong> World");
+    // firstParagraph.html("<strong>Hello</strong> World");
+
+    firstParagraph.html(firstParagraph.html() + " added on the end")
 
 });
