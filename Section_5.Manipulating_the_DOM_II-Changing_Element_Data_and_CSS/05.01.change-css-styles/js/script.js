90c7754b4fde33510c79b56c67ed7083cc7903ab
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-24 14:56:31 -0600

    changing the CSS properties of elements

--- Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.01.change-css-styles/js/script.js
+++ Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.01.change-css-styles/js/script.js
@@ -1,3 +1,20 @@
 $(function () {
+    $(".gallery").css("display", "none"); //hide()
 
+    var redBox = $(".red-box");
+    console.log(redBox.css("width"));
+    console.log(redBox.width());
+
+    redBox.css("background-color", "#AA7700")
+    $("p").css("font-size", "18px");
+    redBox.css("width", "+=20px");
+
+    var properties = $("p").css(["font-size", "line-height", "color"]);
+    console.log(properties)
+    console.log(properties["font-size"])
+
+    redBox.css("user-select", "none");
+    redBox.css("user-select", function() {
+        return "none";
+    }); //you can also use a function
 });
