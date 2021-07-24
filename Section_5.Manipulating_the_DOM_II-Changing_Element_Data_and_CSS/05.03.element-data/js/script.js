34ff40bfebd02ecf99be7c73fc4bb57de247a7c2
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-24 15:45:45 -0600

    Changing the data of an element

--- Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.03.element-data/js/script.js
+++ Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.03.element-data/js/script.js
@@ -1,3 +1,19 @@
 $(function () {
+    var gallery = $(".gallery");
 
+    var images = [
+        "images/laptop-mobile_small.jpg",
+        "images/laptop-on-table_small.jpg",
+        "images/people-office-group-team_small.jpg",
+    ];
+
+    gallery.data("availableImages", images);
+    console.log(gallery.data("availableImages"));
+    gallery.data("name", "The Awesome Gallery");
+    console.log(gallery.data());
+    gallery.removeData("name");
+    console.log(gallery.data("name"));
+
+    var firstParagraph = $("p:first");
+    console.log(firstParagraph.data("mydata"));
 });
