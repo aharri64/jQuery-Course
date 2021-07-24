935fb235b7b679557e96a6ab13d93fd2809b6516
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-24 15:30:43 -0600

    Changing the data of an element

--- Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.02.add-remove-css-classes/js/script.js
+++ Section_5.Manipulating_the_DOM_II-Changing_Element_Data_and_CSS/05.02.add-remove-css-classes/js/script.js
@@ -1,3 +1,18 @@
 $(function () {
+    $("a").addClass("fancy-link");
+    $("p:first").addClass("first large emphasize"); //multiple
 
+    $("li li").addClass(function(index) {
+        $(this).addClass("item-" + index);
+    })
+
+    $("div").addClass(function(index, currentClass) {
+        if (currentClass === "dummy") {
+            return "red-box";
+        }
+    })
+
+    // $(".red-box").removeClass();
+    // $(".red-box").removeClass().addClass("blue-box");
+    $(".dummy").addClass("green-box");
 });
