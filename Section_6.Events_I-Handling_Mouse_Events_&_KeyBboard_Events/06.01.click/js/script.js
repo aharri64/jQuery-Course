0a31b7aa0898478ace2f6d66ffe13511dc9c11d3
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-05 21:19:11 -0600

    add click handlers

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.01.click/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.01.click/js/script.js
@@ -1,3 +1,11 @@
 $(function () {
+    $("#btn-click").click(function(event) {
+        console.log(event);
+        alert("Ten years will be added to your life");
+    });
 
+    $(".red-box").click(function(){
+        $(this).fadeTo(500, 0.5)
+    })
+    $(".red-box").click()
 });
