1ca36987153bad3715791f6a24deae97a2abfe1f
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-05 21:43:44 -0600

    hover handlers

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.02.hover/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.02.hover/js/script.js
@@ -1,3 +1,9 @@
 $(function () {
+    $("#button").hover(function(){
+       alert("button was hovered"); 
+    });
 
+    $(".green-box").hover(function(){
+        $(this).text("I was hovered")
+    })
 });
