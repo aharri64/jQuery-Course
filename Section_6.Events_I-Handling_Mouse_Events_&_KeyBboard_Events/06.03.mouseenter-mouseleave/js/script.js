a6d9b5eed65845a48fb83bc61c1db7602c080980
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-05 22:04:17 -0600

    mouse enter and mouse leave

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.03.mouseenter-mouseleave/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.03.mouseenter-mouseleave/js/script.js
@@ -1,3 +1,17 @@
 $(function () {
+    var blueBox = $(".blue-box")
 
+    // blueBox.mouseenter(function () {
+    //     $(this).stop().fadeTo(500, 0.7);
+    // });
+
+    // blueBox.mouseleave(function () {
+    //     $(this).stop().fadeTo(500, 1);
+    // });
+
+    blueBox.hover(function(){
+        $(this).stop().fadeTo(500, 0.5);
+    }, function(){
+        $(this).stop().fadeTo(500, 1);
+    })
 });
