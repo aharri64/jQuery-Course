23103615802d7de5a3e23c0433fa95e354e88767
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-09 18:57:54 -0600

    Passing Additional Data to Events

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.07.event-data/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.07.event-data/js/script.js
@@ -1,3 +1,15 @@
 $(function () {
+    $("#button").click({
+        user: "Amir",
+        domain: "amirharrison.com"
+    }, function(event) {
+        greetUser(event.data);
+    })
 
+    function greetUser(userdata) {
+        username = userdata.user || "Anonymous";
+        domain = userdata.domain || "example.com";
+
+        alert("Welcome back " + username + " from " + domain + "!");
+    }
 });
