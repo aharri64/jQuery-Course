0602214dec5d006faeef5ff180c4441cd10defe7
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-10 16:43:46 -0600

    focus and blur events

--- Section_7.Events_II-Forms/07.01.focus-blur/js/script.js
+++ Section_7.Events_II-Forms/07.01.focus-blur/js/script.js
@@ -1,4 +1,19 @@
 $(function () {
+  var inputFields = $("input:text, input:password, textarea");
+  // inputFields.focus(function() {
+  //   $(this).css("box-shadow", "0 0 14px #666");
+  // });
+  
+  // inputFields.blur(function() {
+  //   $(this).css("box-shadow", "none");
+  // });
 
-
+  $("#name").blur(function () {
+    var text = $(this).val();
+    if (text.trim().length < 3) {
+      $(this).css("box-shadow", "0 0 14px #911");
+    } else {
+      $(this).css("box-shadow", "0 0 14px #191");
+    }
+  });
 });
