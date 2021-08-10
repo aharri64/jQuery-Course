e7ae1afa275d49d7844dbe445d4de0286d72e08d
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-09 18:47:41 -0600

    delegated events

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.06.delegated-events/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.06.delegated-events/js/script.js
@@ -1,3 +1,20 @@
 $(function () {
+    // $("p").click(function() {
+    //     $(this).slideUp();
+    // });
+    // $("#content").append("<p>This is a dynamically added paragraph.</p>");
 
+    // Delegated Events
+    $("#content").on("click", "p", function() {
+        $(this).slideUp();
+    });
+    $("#content").append("<p>This is a dynamically added paragraph.</p>");
+
+    $("body").on("mouseenter", "li", function() {
+        $(this).css("color", "pink");
+    })
+    $("body").on("mouseleave", "li", function() {
+        $(this).css("color", "black");
+    })
 });
+
