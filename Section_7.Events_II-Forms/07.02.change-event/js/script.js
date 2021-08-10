bf505da5e49bf985fea1510cd941aab67afd10f9
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-10 17:02:49 -0600

    Using the Change Event

--- Section_7.Events_II-Forms/07.02.change-event/js/script.js
+++ Section_7.Events_II-Forms/07.02.change-event/js/script.js
@@ -1,3 +1,14 @@
 $(function () {
-
+    $("#checkbox").change(function() {
+        var isChecked = $(this).is(":checked"); //  alternative = .prop("checked")
+        if (isChecked) {
+            $(this).add("label[for='checkbox']").css("box-shadow", "0 0 4px #181");
+        } else {
+            $(this).add("label[for='checkbox']").css("box-shadow", "0 0 4px #811");
+        }
+    });
+    $("#selection").change(function () {
+        var chosen = $(this).find(":selected").text();
+        alert(chosen);
+    });
 });
