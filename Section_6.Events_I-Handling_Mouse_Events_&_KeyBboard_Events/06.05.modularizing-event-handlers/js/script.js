9d3130c8d5605e295b203669077301db59fe983e
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-09 18:26:09 -0600

    Modularizing Event Handlers (No More Inline Functions)

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.05.modularizing-event-handlers/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.05.modularizing-event-handlers/js/script.js
@@ -1,6 +1,25 @@
 $(function () {
+    // function logEvent() {
+    //     console.log("Mouse was clicked or key was pressed")
+    // }
 
+    // $("html").on("click keydown" logEvent);
+    galleryImage = $(".gallery").find("img")
+    var images = [
+        "images/laptop-mobile_small.jpg",
+        "images/laptop-on-table_small.jpg",
+        "images/people-office-group-team_small.jpg",
+    ];
 
+    var i = 0;
 
+    galleryImage.on("click", switchImage);
+
+    function switchImage() {
+        i= (i + 1) % images.length;
+        $(galleryImage).fadeOut(function() {
+            $(galleryImage).attr("src", images[i]).fadeIn();
+        });
+    }
 });
 
