24ddec3a7d8db9b5ad2fa87a64d37fc454f54d46
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-09 21:37:28 -0600

    Coding Activity: Creating an Image Gallery with Lightbox Preview

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.08.gallery-with-lightbox/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.08.gallery-with-lightbox/js/script.js
@@ -1,4 +1,22 @@
 $(function () {
+    var galleryItems = $(".gallery").find("img")
 
+    galleryItems.css("width", "33%").css("opacity", "0.7");
 
+    galleryItems.mouseenter(function() {
+        $(this).stop().fadeTo(500, 1);
+    });
+    galleryItems.mouseleave(function() {
+        $(this).stop().fadeTo(500, 0.7);
+    });
+
+    galleryItems.click(function() {
+        var source = $(this).attr("src");
+        var image = $("<img>").attr("src", source).css("width", "100%");
+        $(".lightbox").empty().append(image).fadeIn(2000);
+    })
+
+    $(".lightbox").click(function() {
+        $(this).stop().fadeOut();
+    });
 });
