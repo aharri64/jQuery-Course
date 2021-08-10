1c948d918759e92373fc9415cfab2423d0fdd2c2
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-09 18:13:09 -0600

    adding the same handler for multiple events

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.04.same-handler-multiple-events/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.04.same-handler-multiple-events/js/script.js
@@ -1,3 +1,23 @@
 $(function () {
+    // .on( "click", function() {...})
+    // $("html").on("click keydown", function() {
+    //     console.log("Mouse was clicked or a key was pressed);
+    // });
+    
+    var galleryImage = $(".gallery").find("img").first();
+    var images = [
+        "images/laptop-mobile_small.jpg",
+        "images/laptop-on-table_small.jpg",
+        "images/people-office-group-team_small.jpg",
+    ];
+
+    var i = 0;
+
+    $(".gallery").find("img").on("click", function() {
+        i= (i + 1) % images.length;
+        $(this).fadeOut(function() {
+            $(this).attr("src", images[i]).fadeIn();
+        });
+    });
 
 });
