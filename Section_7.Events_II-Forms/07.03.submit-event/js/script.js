30e60e7e3da2eb064d5c78cd93a56d9ce211590b
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-10 17:56:42 -0600

    Handling the Submit Event

--- Section_7.Events_II-Forms/07.03.submit-event/js/script.js
+++ Section_7.Events_II-Forms/07.03.submit-event/js/script.js
@@ -1,3 +1,11 @@
 $(function () {
-
+    $("#form").submit(function(event) {
+        var textarea = $("#message")
+        if (textarea.val().trim() == '') {
+            textarea.css("box-shadow", "0 0 24px #811");
+            event.preventDefault();
+        } else {
+            //form will be submitted
+        }
+    });
 });
