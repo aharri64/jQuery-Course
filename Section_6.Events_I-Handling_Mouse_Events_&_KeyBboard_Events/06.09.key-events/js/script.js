81dbf7faeafd98beefdac4d71c650d4700a9a10d
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-09 22:48:20 -0600

    Handling KeyDown & KeyUp Events

--- Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.09.key-events/js/script.js
+++ Section_6.Events_I-Handling_Mouse_Events_&_KeyBboard_Events/06.09.key-events/js/script.js
@@ -1,4 +1,42 @@
 $(function () {
+    // keypress() EVIL!
+    $("html").keydown(function(event) {
+        console.log(event.which)
+    });
 
+    var ARROW_RIGHT = 39;
+    var ARROW_LEFT = 37;
+    var ARROW_DOWN = 40;
+    var ARROW_UP = 38;
+    $("html").keydown(function(event) {
+        if (event.which == ARROW_RIGHT) {
+            $(".blue-box").stop().animate({
+                marginLeft: "+=10px"
+            }, 50);
+        }
+    });
 
+    $("html").keydown(function(event) {
+        if (event.which == ARROW_LEFT) {
+            $(".blue-box").stop().animate({
+                marginLeft: "-=10px"
+            }, 50);
+        }
+    });
+
+    $("html").keydown(function(event) {
+        if (event.which == ARROW_UP) {
+            $(".blue-box").stop().animate({
+                marginTop: "-=10px"
+            }, 50);
+        }
+    });
+
+    $("html").keydown(function(event) {
+        if (event.which == ARROW_DOWN) {
+            $(".blue-box").stop().animate({
+                marginTop: "+=10px"
+            }, 50);
+        }
+    });
 });
