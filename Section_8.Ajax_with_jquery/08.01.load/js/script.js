8d12037b1cc6d2ec4f240bf1165bb1af9aa19c99
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-11 10:19:55 -0600

    Fetching a Server File with jQuery

--- Section_8.Ajax_with_jquery/08.01.load/js/script.js
+++ Section_8.Ajax_with_jquery/08.01.load/js/script.js
@@ -1,5 +1,11 @@
 $(function () {
+    // $.load()
+    $("#code").load("js/script.js");
 
-
-
+    $("#code").load("js/script.js", function(response, status) {
+        if (status == "error") {
+            alert("Could not find file")
+        }
+        console.log(response);
+    });
 });
