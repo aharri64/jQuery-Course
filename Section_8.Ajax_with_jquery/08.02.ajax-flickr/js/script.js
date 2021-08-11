4c971d156516575eac71d0b27da847a8e0484cde
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-11 14:31:11 -0600

    flicker

--- Section_8.Ajax_with_jquery/08.02.ajax-flickr/js/script.js
+++ Section_8.Ajax_with_jquery/08.02.ajax-flickr/js/script.js
@@ -1,4 +1,23 @@
 $(function () {
+    var flickrApiUrl = "https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
+    $.getJSON(flickrApiUrl, {
+        // options...
+        tags: "sun, beach",
+        tagmode: "any",
+        format: "json"
+    }).done(function(data) {
+        // Success
+        console.log(data);
+        $.each(data.items, function(index, item) {
+            console.log(item)
+            $("<img>").attr("src", item.media.m).appendTo("#flickr");
 
+            if (index == 4) {
+                return false;
+            }
+        })
+    }).fail(function() {
+        alert("ajax call failed")
+    })
 });
