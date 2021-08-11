08ad0a077bab79c33e67e2b4d6fd447dbfe406f7
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-10 18:32:07 -0600

    Coding Activity: Complete Form Validation on Submit

--- Section_7.Events_II-Forms/07.04.form-validation/js/script.js
+++ Section_7.Events_II-Forms/07.04.form-validation/js/script.js
@@ -1,3 +1,63 @@
 $(function () {
+    $("#form").submit(function(event) {
+        var name = $("#name").val()
+        var password = $("#password").val()
+        var message = $("#message").val()
+        var checked = $("#checkbox").val()
 
+        validateNameField(name, event);
+        validatePasswordField(password, event);
+        validateMessageField(message, event);
+        validateCheckboxField(checked, event)
+    });
 });
+
+function isValidName(name) {
+    return name.trim().length >= 2;
+}
+
+function isValidPassword(password) {
+    return password.length >= 6 && /.*[0-9].*/.test(password);
+}   
+
+function isValidMessage(message) {
+    return message.trim() !== "";
+}
+
+function validateNameField(name, event) {
+    if (!isValidName(name)) {
+        $("#name-feedback").text("Please enter at least two characters");
+        event.preventDefault();
+    } else {
+        $("#name-feedback").text("");
+    }
+}
+
+function validatePasswordField(password, event) {
+    if (!isValidPassword(password)) {
+        $("#password-feedback").text(
+        "The password should have at least 6 characters and contain a number"
+        );
+        event.preventDefault();
+    } else {
+        $("#password-feedback").text("");
+    }
+}
+
+function validateMessageField(message, event) {
+    if (!isValidMessage(message)) {
+        $("#message-feedback").text("Please enter a message.");
+        event.preventDefault();
+    } else {
+        $("#message-feedback").text("");
+    }
+}
+
+function validateCheckboxField(isChecked, event) {
+    if (!isChecked) {
+        $("#checkbox-feedback").text("Please agree to this.");
+        event.preventDefault();
+    } else {
+        $("#checkbox-feedback").text("");
+    }
+}
