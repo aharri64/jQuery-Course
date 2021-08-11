57421d0e20711feb4bafc86ff8e280c2d90c36ac
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-11 16:46:20 -0600

    Coding Activity: Retrieving Pokémon Data from The PokéAPI (Or Star Wars)

--- Section_8.Ajax_with_jquery/08.03.ajax-pokemon/js/script.js
+++ Section_8.Ajax_with_jquery/08.03.ajax-pokemon/js/script.js
@@ -1,3 +1,20 @@
-$(function () {
+// https://github.com/public-apis/public-apis
+// https://pokeapi.co/
+// https://pipedream.com/apps/swapi
 
-};
+$(function () {
+    var pokeApiUrl = "https://pokeapi.co/api/v2/generation/1/";
+
+    $.getJSON(pokeApiUrl).done(function(data) {
+        console.log(data);
+        $.each(data.pokemon_species, function(index, pokemon) {
+            var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+            var par = $("<p>").html("Pokemon species no. " + (index+1) + " is " + name);
+            par.appendTo("#pokedex");
+        });
+    }).fail(function() {
+        console.log("Request to Pokeapi failed.");
+    }).always(function() {
+        console.log("Pokemon is awesome!")
+    });
+});
